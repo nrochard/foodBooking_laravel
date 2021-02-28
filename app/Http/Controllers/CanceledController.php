39c968c97b22e47f1943bb78c4579d71935b7be6
Noémie ROCHARD <nrochard@student.42.fr>
2021-02-28 23:14:31 +0100

    check token to cancel ok

--- app/Http/Controllers/CanceledController.php
+++ app/Http/Controllers/CanceledController.php
@@ -2,10 +2,17 @@
 
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class CanceledController extends Controller
 {
-    public function show(){
+    public function show($token){
+
+        if (DB::table('booking')->where('token', $token)->doesntExist()) {
+            return redirect('/reservation')
+            ->with('error',"Il n'y a pas de réservation qui correspond à votre demande ! ");
+        }
         return view('canceled');
     }
 }
