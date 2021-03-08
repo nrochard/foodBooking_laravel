5127ed482921baba024431e63e70dfd8d87cb3f1
Noémie ROCHARD <nrochard@student.42.fr>
2021-03-08 12:06:14 +0100

    add config info

--- config/informations.php
+++ config/informations.php
@@ -1,8 +1,11 @@
 <?php 
 
 return [
+    'name' => "Food Booking",
     'date' => "Du lundi au vendredi",
     'slot' => "De 10h30 à 13h et de 16h à 18h",
     'time' => "30 minutes",
     'limit' => 5,
+    'phone' => "02 93 04 28 57",
+    'mail' => "hello@foodbooking.com",
 ];
